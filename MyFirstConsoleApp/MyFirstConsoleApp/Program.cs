f454afdf9f844055893473edc7b2a8e52bb85ead
Dominik Gałenza <dominik.galenza@gmail.com>
2023-03-05 17:49:46 +0100

    Completed the second chapter.

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -6,7 +6,77 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OperatorExamples();
+            TryAnIf();
+            TryAnIfElse();
+            TrySomeLoops();
+        }
+      
+        private static void OperatorExamples()
+        {
+            int width = 3;
+            width++;
+
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area);
+
+            while (area < 50)
+            {
+                height++;
+                area = width * height;
+            }
+
+            do
+            {
+                width--;
+                area = width * height;
+            } while (area > 25);
+
+            string result = "The area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
+        }
+
+        private static void TryAnIf()
+        {
+            int someValue = 4;
+            string name = "Bobbo Jr.";
+            if (someValue == 3 && name == "Joe")
+            {
+                Console.WriteLine("x is 3 and the name is Joe");
+            }
+            Console.WriteLine("This line runs no matter what");
+        }
+
+        private static void TryAnIfElse()
+        {
+            int x = 5;
+            if (x == 10)
+            {
+                Console.WriteLine("x must be 10");
+            }
+            else
+            {
+                Console.WriteLine("x isn't 10");
+            }
+        }
+
+        private static void TrySomeLoops()
+        {
+            int count = 0;
+            while (count < 10)
+            {
+                count++;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                count--;
+            }
+            Console.WriteLine("The answer is " + count);
         }
     }
 }

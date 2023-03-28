b6d285b2a19ff54946fcdf3df2e9bd958f96f95e
Dominik Gałenza <dominik.galenza@gmail.com>
2023-03-28 18:57:37 +0200

    Finished the exercise regarding downcasting.

---
+++ Chapter 7/Animals/Animals/ISwimmer.cs
@@ -0,0 +1,4 @@
+﻿interface ISwimmer
+{
+    void Swim();
+}
---
+++ Chapter 7/Animals/Animals/Program.cs
@@ -0,0 +1,25 @@
+﻿Animal[] animals =
+{
+    new Wolf(false),
+    new Hippo(),
+    new Wolf(true),
+    new Wolf(false),
+    new Hippo()
+};
+
+foreach (Animal animal in animals)
+{
+    animal.MakeNoise();
+
+    if (animal is ISwimmer swimmer)
+    {
+        swimmer.Swim();
+    }
+
+    if (animal is IPackHunter hunter)
+    {
+        hunter.HuntInPack();
+    }
+
+    Console.WriteLine();
+}

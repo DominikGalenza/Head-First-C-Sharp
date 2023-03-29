fef12aecde1929c2453c9ac15e8eab7db9670992
Dominik Gałenza <dominik.galenza@gmail.com>
2023-03-29 21:47:31 +0200

    Extended previous exercise using static field and default implementation in the interface.

--- Chapter 7/Clowns/Clowns/Program.cs
+++ Chapter 7/Clowns/Clowns/Program.cs
@@ -1,6 +1,9 @@
-﻿IClown fingersTheClown = new ScaryScary("big red nose", 14);
+﻿IClown.CarCapacity = 18;
+Console.WriteLine(IClown.ClownCarDescription());
+
+IClown fingersTheClown = new ScaryScary("big red nose", 14);
 fingersTheClown.Honk();
 if (fingersTheClown is IScaryClown iScaryClownReference)
 {
-    iScaryClownReference.ScareLittleChildren();
+    iScaryClownReference.ScareAdults();
 }

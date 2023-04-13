6014b46e59997177af35b2304f2635127cc76459
Dominik Gałenza <dominik.galenza@gmail.com>
2023-04-13 20:27:06 +0200

    Finished the first exercise in the eighth chapter.

---
+++ Chapter 8/PlayingCards/PlayingCards/Card.cs
@@ -0,0 +1,18 @@
+﻿class Card
+{
+    public Values Value { get; private set; }
+    public Suits Suit { get; private set; }
+    public string Name 
+    { 
+        get
+        {
+            return $"{Value} of {Suit}";
+        }
+    }
+
+    public Card(Values value, Suits suit)
+    {
+        Value = value;
+        Suit = suit;
+    }
+}

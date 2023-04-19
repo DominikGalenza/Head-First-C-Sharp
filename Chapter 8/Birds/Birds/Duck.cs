14bc251525683343c509d7249eaf6b270b98a8b9
Dominik Gałenza <dominik.galenza@gmail.com>
2023-04-19 18:54:51 +0200

    Finished the exercise regarding covariance.

---
+++ Chapter 8/Birds/Birds/Duck.cs
@@ -0,0 +1,10 @@
+﻿class Duck : Bird
+{
+    public int Size { get; set; }
+    public KindOfDuck Kind { get; set; }
+
+    public override string ToString()
+    {
+        return $"A {Size} inch {Kind}";
+    }
+}

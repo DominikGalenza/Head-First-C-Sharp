1791267ccb3382ed83fb6f1fa50c0069b505d91e
Dominik Gałenza <dominik.galenza@gmail.com>
2023-05-01 20:09:25 +0200

    Finished the exercise regarding dictionaries.

---
+++ Chapter 8/Yankees/Yankees/RetiredPlayer.cs
@@ -0,0 +1,11 @@
+﻿class RetiredPlayer
+{
+    public string Name { get; set; }
+    public int YearRetired { get; set; }
+
+    public RetiredPlayer(string player, int yearRetired)
+    {
+        Name = player;
+        YearRetired = yearRetired;
+    }
+}

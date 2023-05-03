b33fd4e95b85879219cefea904548d0e2ee52cac
Dominik Gałenza <dominik.galenza@gmail.com>
2023-05-03 12:25:41 +0200

    Finished the last exercise in the chapter, regarding lists.

---
+++ Chapter 8/TwoDecksBlazor/TwoDecksBlazor/TwoDecks.cs
@@ -0,0 +1,69 @@
+﻿namespace TwoDecksBlazor
+{
+	class TwoDecks
+	{
+		public int LeftDeckCount 
+		{
+			get
+			{
+				return leftDeck.Count;
+			}
+		}
+		public int RightDeckCount 
+		{ 
+			get
+			{
+				return rightDeck.Count;
+			}
+		}
+		public int LeftCardSelected { get; set; }
+		public int RightCardSelected { get; set; }
+
+		private Deck leftDeck = new Deck();
+		private Deck rightDeck = new Deck();
+
+		public string LeftDeckCardName(int i)
+		{
+			return leftDeck[i].ToString();
+		}
+
+		public string RightDeckCardName(int i)
+		{
+			return rightDeck[i].ToString();
+		}
+
+		public void Shuffle()
+		{
+			leftDeck.Shuffle();
+		}
+
+		public void Reset()
+		{
+			leftDeck = new Deck();
+		}
+
+		public void Sort()
+		{
+			rightDeck.Sort(new CardComparerByValue());
+		}
+
+		public void Clear()
+		{
+			rightDeck.Clear();
+		}
+
+		public void MoveCard(Direction direction)
+		{
+			if (direction == Direction.LeftToRight)
+			{
+				rightDeck.Add(leftDeck[LeftCardSelected]);
+				leftDeck.RemoveAt(LeftCardSelected);
+			}
+			else
+			{
+				leftDeck.Add(rightDeck[RightCardSelected]);
+				rightDeck.RemoveAt(RightCardSelected);
+			}
+		}
+	}
+}

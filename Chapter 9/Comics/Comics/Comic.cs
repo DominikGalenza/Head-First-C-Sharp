6daa7cce5c669cb460305f0edc83a96fc8752f74
Dominik Gałenza <dominik.galenza@gmail.com>
2023-05-06 19:55:14 +0200

    Finished the second exercise regarding LINQ.

---
+++ Chapter 9/Comics/Comics/Comic.cs
@@ -0,0 +1,32 @@
+﻿namespace Comics
+{
+	class Comic
+	{
+		public static readonly IEnumerable<Comic> Catalog = new List<Comic>
+		{
+			new Comic { Name = "Johnny America vs. the Pinko", Issue = 6 },
+			new Comic { Name = "Rock and Roll (limited edition)", Issue = 19 },
+			new Comic { Name = "Woman's Work", Issue = 36 },
+			new Comic { Name = "Hippie Madness (misprinted)", Issue = 57 },
+			new Comic { Name = "Revenge of the New Wave Freak (damaged)", Issue = 68 },
+			new Comic { Name = "Black Monday", Issue = 74 },
+			new Comic { Name = "Tribal Tattoo Mandess", Issue = 83 },
+			new Comic { Name = "The Death of the Object", Issue = 97 }
+		};
+		public static readonly IReadOnlyDictionary<int, decimal> Prices = new Dictionary<int, decimal>
+		{
+			{ 6, 3600m },
+			{ 19, 500m },
+			{ 36, 650m },
+			{ 57, 13525m },
+			{ 68, 250m },
+			{ 74, 75m },
+			{ 83, 25.75m },
+			{ 97, 35.25m }
+		};
+		public string Name { get; set; }
+		public int Issue { get; set; }
+
+		public override string ToString() => $"{Name} (Issue #{Issue})";
+	}
+}
---
+++ Chapter 9/Comics/Comics/Program.cs
@@ -0,0 +1,7 @@
+﻿using Comics;
+
+IEnumerable<string> mostExpensive = 
+	from comic in Comic.Catalog
+	where Comic.Prices[comic.Issue] > 500
+	orderby Comic.Prices[comic.Issue] descending
+	select $"{comic} is worth {Comic.Prices[comic.Issue]:c}";

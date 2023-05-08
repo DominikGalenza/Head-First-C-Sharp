87ec9c273499420bf45f234ade97c6aad0ce2e29
Dominik Gałenza <dominik.galenza@gmail.com>
2023-05-08 21:50:44 +0200

    Finished the third exercise regarding LINQ.

---
+++ Chapter 9/CardLinq/CardLinq/Deck.cs
@@ -0,0 +1,45 @@
+﻿namespace CardLinq
+{
+	class Deck : List<Card>
+	{
+		private static Random random = new Random();
+
+		public Deck()
+		{
+			Reset();
+		}
+
+		public void Reset()
+		{
+			Clear();
+			for (int suit = 0; suit <= 3; suit++)
+			{
+				for (int value = 0; value <= 13; value++)
+				{
+					Add(new Card((Values)value, (Suits)suit));
+				}
+			}
+		}
+
+		public Deck Shuffle()
+		{
+			List<Card> copy = new List<Card>(this);
+			Clear();
+			while (copy.Count > 0)
+			{
+				int index = random.Next(copy.Count);
+				Card card = copy[index];
+				copy.RemoveAt(index);
+				Add(card);
+			}
+			return this;
+		}
+
+		public Card Deal(int index)
+		{
+			Card cardToDeal = base[index];
+			RemoveAt(index);
+			return cardToDeal;
+		}
+	}
+}

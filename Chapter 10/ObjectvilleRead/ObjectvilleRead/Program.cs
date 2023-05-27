f5e66584309af437a227033740d41f82592e7495
Dominik Gałenza <dominik.galenza@gmail.com>
2023-05-27 18:53:26 +0200

    Finished the exercise regarding stream reader.

---
+++ Chapter 10/ObjectvilleRead/ObjectvilleRead/Program.cs
@@ -0,0 +1,20 @@
+﻿string folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+StreamReader reader = new StreamReader($"{folder}{Path.DirectorySeparatorChar}secret_plan.txt");
+StreamWriter writer = new StreamWriter($"{folder}{Path.DirectorySeparatorChar}emailToCaptainA.txt");
+
+writer.WriteLine("To: CaptainAmazing@objectville.net");
+writer.WriteLine("From: Commissioner@objectville.net");
+writer.WriteLine("Subject: Can you save the day... again?");
+writer.WriteLine();
+writer.WriteLine("We've discovered the Swindler's terrible plan:");
+
+while (!reader.EndOfStream)
+{
+	string? lineFromThePlan = reader.ReadLine();
+	writer.WriteLine($"the plan -> {lineFromThePlan}");
+}
+writer.WriteLine();
+writer.WriteLine("Can you help us?");
+
+writer.Close();
+reader.Close();

877c4d01d10c8df668c7528dbbd931781eafcd40
Dominik Gałenza <dominik.galenza@gmail.com>
2023-05-27 17:23:03 +0200

    Finished the exercise regarding stream writer.

---
+++ Chapter 10/Objectville/Objectville/Program.cs
@@ -0,0 +1,14 @@
+﻿StreamWriter streamWriter = new StreamWriter("secret_plan.txt");
+
+streamWriter.WriteLine("How I'll defeat Captain Amazing");
+streamWriter.WriteLine("Another genius secret plan by The Swindler");
+streamWriter.WriteLine("I'll unleash my army of clones upon the citizens of Objectville.");
+
+string location = "the mall";
+for (int number = 1; number <= 5; number++)
+{
+	streamWriter.WriteLine($"Clone #{number} attacks {location}");
+	location = (location == "the mall") ? "downtown" : "the mall";
+}
+
+streamWriter.Close();

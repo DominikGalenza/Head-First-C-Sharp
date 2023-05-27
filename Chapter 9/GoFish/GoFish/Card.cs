7910b0435502c3a7dce32628ce701d940a2a50e4
Dominik Gałenza <dominik.galenza@gmail.com>
2023-05-27 14:16:09 +0200

    Finished the last exercise in the ninth chapter.

---
+++ Chapter 9/GoFish/GoFish/Card.cs
@@ -0,0 +1,31 @@
+﻿namespace CardLinq
+{
+    public class Card : IComparable<Card>
+    {
+        public Values Value { get; private set; }
+        public Suits Suit { get; private set; }
+        public string Name
+        {
+            get
+            {
+                return $"{Value} of {Suit}";
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+		public int CompareTo(Card other)
+		{
+			return new CardComparerByValue().Compare(this, other);
+		}
+
+		public Card(Values value, Suits suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+    }
+}
---
+++ Chapter 9/GoFish/GoFish/Suits.cs
@@ -0,0 +1,10 @@
+﻿namespace CardLinq
+{
+    public enum Suits
+    {
+        Diamonds,
+        Clubs,
+        Hearts,
+        Spades
+    }
+}

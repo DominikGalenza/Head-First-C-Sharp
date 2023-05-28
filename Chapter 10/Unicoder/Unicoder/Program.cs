1270c1ff511bf5fb024c55d7b93fd20939100e47
Dominik Gałenza <dominik.galenza@gmail.com>
2023-05-28 20:02:57 +0200

    Finished the exercise regarding Unicode.

---
+++ Chapter 10/Unicoder/Unicoder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Text.Json;
+
+//File.WriteAllText("Eureka.txt", "Eureka!");
+File.WriteAllText("Eureka.txt", "שלום", Encoding.Unicode);
+byte[] eurekaBytes = File.ReadAllBytes("Eureka.txt");
+
+foreach (byte b in eurekaBytes)
+{
+	Console.Write($"{b} ");
+}
+Console.WriteLine(Encoding.UTF8.GetString(eurekaBytes));
+
+foreach (byte b in eurekaBytes)
+{
+	Console.Write($"{b:x2} ");
+}
+Console.WriteLine();
+
+Console.WriteLine(JsonSerializer.Serialize("ש"));
+
+File.WriteAllText("Elephant1.txt", "\uD83D\uDC18");
+File.WriteAllText("Elephant2.txt", "\U0001F418");

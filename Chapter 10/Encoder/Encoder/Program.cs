aa109f7ea2a310d3fe49478adf946815e4f4884c
Dominik Gałenza <dominik.galenza@gmail.com>
2023-05-28 12:21:16 +0200

    Finished the exercise regarding using statement.

---
+++ Chapter 10/Encoder/Encoder/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+using (var ms = new MemoryStream())
+{
+	using (var sw = new StreamWriter(ms))
+	{
+		sw.WriteLine($"The value is {123.45678:0.00}");
+	}
+	Console.WriteLine(Encoding.UTF8.GetString(ms.ToArray()));
+}

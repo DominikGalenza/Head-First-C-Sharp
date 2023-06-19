50f45d693545a29d9bc5945d594bbdc811133a14
Dominik Gałenza <dominik.galenza@gmail.com>
2023-06-19 22:41:10 +0200

    Finished the exercise regarding structs.

---
+++ Chapter 11/Dogs/Dogs/Canine.cs
@@ -0,0 +1,19 @@
+﻿namespace Dogs
+{
+	public class Canine
+	{
+		public string Name { get; set; }
+		public string Breed { get; set; }
+
+		public Canine(string name, string breed)
+		{
+			Name = name;
+			Breed = breed;
+		}
+
+		public void Speak()
+		{
+			Console.WriteLine($"My name is {Name} and I'm a {Breed}");
+		}
+	}
+}
---
+++ Chapter 11/Dogs/Dogs/Dog.cs
@@ -0,0 +1,19 @@
+﻿namespace Dogs
+{
+	public struct Dog
+	{
+		public string Name { get; set; }
+		public string Breed { get; set; }
+
+		public Dog(string name, string breed)
+		{
+			Name = name;
+			Breed = breed;
+		}
+
+		public void Speak()
+		{
+			Console.WriteLine($"My name is {Name} and I'm a {Breed}");
+		}
+	}
+}

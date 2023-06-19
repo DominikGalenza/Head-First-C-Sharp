94449defc90dd08f50980406191c44025ac0ceb8
Dominik Gałenza <dominik.galenza@gmail.com>
2023-06-19 19:22:05 +0200

    Finished the exercise regarding finalizer (destructor).

---
+++ Chapter 11/Clones/Clones/EvilClone.cs
@@ -0,0 +1,14 @@
+﻿namespace Clones
+{
+	internal class EvilClone
+	{
+		public static int CloneCount = 0;
+		public int CloneID { get; } = ++CloneCount;
+		public EvilClone() => Console.WriteLine($"Clone #{CloneID} is wreaking havoc");
+
+		~EvilClone()
+		{
+			Console.WriteLine($"Clone #{CloneID} destroyed");
+		}
+	}
+}
---
+++ Chapter 11/Clones/Clones/Program.cs
@@ -0,0 +1,22 @@
+﻿using Clones;
+
+System.Diagnostics.Stopwatch stopwatch = System.Diagnostics.Stopwatch.StartNew();
+List<EvilClone> clones = new List<EvilClone>();
+
+while (true)
+{
+	switch (Console.ReadKey(true).KeyChar)
+	{
+		case 'a':
+			clones.Add(new EvilClone());
+			break;
+		case 'c':
+			Console.WriteLine($"Clearing list at time {stopwatch.ElapsedMilliseconds}");
+			clones.Clear();
+			break;
+		case 'g':
+			Console.WriteLine($"Collecting at time {stopwatch.ElapsedMilliseconds}");
+			GC.Collect();
+			break;
+	}
+}

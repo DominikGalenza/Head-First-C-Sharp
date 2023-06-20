6a73647c721ac41383096ed7538b020f60b8278f
Dominik Gałenza <dominik.galenza@gmail.com>
2023-06-20 18:57:34 +0200

    Finished the exercise regarding out parameters.

---
+++ Chapter 11/ThreeValues/ThreeValues/Program.cs
@@ -0,0 +1,14 @@
+﻿static int ReturnThreeValues(int value, out double half, out int twice)
+{
+	half = value / 2f;
+	twice = value * 2;
+	return value + 1;
+}
+
+Console.Write("Enter a number: ");
+if (int.TryParse(Console.ReadLine(), out int input))
+{
+	var output1 = ReturnThreeValues(input, out double output2, out int output3);
+
+	Console.WriteLine($"Outputs: plus one = {output1}, half = {output2:F}, twice = {output3}");
+}

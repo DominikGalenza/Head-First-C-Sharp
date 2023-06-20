881d540e91af794576f07608418269e585437c34
Dominik Gałenza <dominik.galenza@gmail.com>
2023-06-20 22:47:50 +0200

    Finished the exercise regarding optional parameters.

---
+++ Chapter 11/OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,15 @@
+﻿static void CheckTemperature(double temperature, double tooHigh = 99.5, double tooLow = 96.5)
+{
+	if (temperature < tooHigh && temperature > tooLow)
+	{
+		Console.WriteLine($"{temperature} degrees F - feeling good!");
+	}
+	else
+	{
+		Console.WriteLine($"Uh-oh {temperature} degrees F -- better see a doctor!");
+	}
+}
+
+CheckTemperature(101.3);
+CheckTemperature(101.3, 102.5, 100.5);
+CheckTemperature(96.2, tooLow: 95.5);

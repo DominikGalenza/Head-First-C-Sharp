8e4db42a7282bf68e96ff62b60eb2c40da8ab6e2
Dominik Gałenza <dominik.galenza@gmail.com>
2023-06-20 19:18:33 +0200

    Finished the exercise regarding ref parameters.

---
+++ Chapter 11/PassingByReference/PassingByReference/Program.cs
@@ -0,0 +1,25 @@
+﻿class Guy
+{
+	public string Name { get; set; }
+	public int Age { get; set; }
+	public override string ToString() => $"a {Age}-year-old named {Name}";
+}
+
+class Program
+{
+	static void ModifyAnIntAndGuy(ref int valueReference, ref Guy guyReference)
+	{
+		valueReference += 10;
+		guyReference.Name = "Bob";
+		guyReference.Age = 37;
+	}
+
+	static void Main(string[] args)
+	{
+		int i = 1;
+		Guy guy = new Guy() { Name = "Joe", Age = 26 };
+		Console.WriteLine($"i is {i} and guy is {guy}");
+		ModifyAnIntAndGuy(ref i, ref guy);
+		Console.WriteLine($"Now i is {i} and guy is {guy}");
+	}
+}

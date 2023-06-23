791d6b7b3668198b8f01d1ba0a66f05ab69a9ed7
Dominik Gałenza <dominik.galenza@gmail.com>
2023-06-23 19:38:49 +0200

    Finished the exercise regarding handling exceptions.

---
+++ Chapter 12/HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿string firstLine = "No first line was read";
+try
+{
+	string[] lines = File.ReadAllLines(args[0]);
+	firstLine = lines.Length > 0 ? lines[0] : "The file was empty";
+}
+catch (IndexOutOfRangeException)
+{
+	Console.Error.WriteLine("Please specify a filename.");
+}
+catch (FileNotFoundException)
+{
+	Console.Error.WriteLine($"Unable to find file: {args[0]}");
+}
+catch (UnauthorizedAccessException exception)
+{
+	Console.Error.WriteLine($"File {args[0]} could not be accessed: {exception}");
+}
+finally
+{
+	Console.WriteLine(firstLine);
+}
